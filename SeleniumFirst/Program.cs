d2510ba5068f995c69fe96843bcc2b88dd688fde
Anton Valenkevych <AValenkevych@inwk.com>
2018-08-14 17:10:02 +0200

    first commit

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -23,10 +24,32 @@ namespace SeleniumFirst
         }
 
         [Test]
-        public void FirstTest()
+        public void CreateNewUserTest()
         {
-            
+            IWebElement SignIn = driver.FindElement(By.CssSelector("#nav-link-accountList > span.nav-line-1"));
+            SignIn.Click();
+
+            IWebElement CreateAccountButton = driver.FindElement(By.CssSelector("#createAccountSubmit"));
+            CreateAccountButton.Click();
+
+            IWebElement YourName = driver.FindElement(By.CssSelector("#ap_customer_name"));
+            YourName.SendKeys("Jon Doe");
+
+            IWebElement Email = driver.FindElement(By.CssSelector("#ap_email"));
+            Email.SendKeys("jondoe999@yopmail.com");
+
+            IWebElement Password = driver.FindElement(By.CssSelector("#ap_password"));
+            Password.SendKeys("zPau7ZXr");
            
+
+            IWebElement Renterpass = driver.FindElement(By.CssSelector("#ap_password_check"));
+            Renterpass.SendKeys("zPau7ZXr");
+
+            IWebElement ContinueButton = driver.FindElement(By.CssSelector("#continue"));
+            ContinueButton.Click();
+
+            Thread.Sleep(5000);
+
 
         }
 
