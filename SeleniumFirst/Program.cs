2405f2170d5afb60266b39763e9c1d724835fbb9
Anton Valenkevych <AValenkevych@inwk.com>
2018-08-14 16:34:00 +0200

    Add project files.

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        IWebDriver driver;
+        static void Main(string[] args) { } 
+        
+       [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://amazon.com");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            
+           
+
+        }
+
+        [TearDown]
+        public void End()
+        {
+            driver.Close();
+        }
+    }
+        
+
+        
+
+
+
+
+    }
+
